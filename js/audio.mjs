de3376aeb42808944f1880cd9e9a11519e1a715d
HaiCao <cao.hoang.hai@talentnet.vn>
2024-04-14 19:55:32 +0700

    update content and audio

--- js/audio.mjs
+++ js/audio.mjs
@@ -3,6 +3,21 @@ export const audio = (() => {
     const music = document.getElementById('button-music');
     let audio = null;
 
+    // const getAudio = () => {
+    //     if (!audio) {
+    //         audio = new Audio();
+    //         audio.src = music.getAttribute('data-url');
+    //         audio.load();
+    //         audio.currentTime = 0;
+    //         audio.autoplay = true;
+    //         audio.muted = false;
+    //         audio.loop = true;
+    //         audio.volume = 1;
+    //     }
+
+    //     return audio;
+    // };
+
     const getAudio = () => {
         if (!audio) {
             audio = new Audio();
@@ -12,9 +27,17 @@ export const audio = (() => {
             audio.autoplay = true;
             audio.muted = false;
             audio.loop = true;
-            audio.volume = 1;
+            audio.volume = 0; // Start with volume set to 0
         }
-
+    
+        // Gradually increase volume
+        const interval = setInterval(() => {
+            audio.volume += 0.1; // Increase volume by 10% each time
+            if (audio.volume >= 1) {
+                clearInterval(interval); // Stop increasing volume when it reaches 100%
+            }
+        }, 1000); // Adjust the interval time (in milliseconds) as needed
+    
         return audio;
     };
 
