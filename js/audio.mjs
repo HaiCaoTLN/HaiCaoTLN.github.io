363e37b07d2fa9455d137b6901901fbffad5be8b
HaiCao <cao.hoang.hai@talentnet.vn>
2024-04-23 00:00:31 +0700

    update css

--- js/audio.mjs
+++ js/audio.mjs
@@ -22,39 +22,6 @@ export const audio = (() => {
     window.addEventListener('blur', handleWindowBlur);
     window.addEventListener('focus', handleWindowFocus);
 
-
-    // let isPageVisible = true;
-
-    // // Function to handle visibility change
-    // const handleVisibilityChange = () => {
-    //     if (document.visibilityState === 'hidden') {
-    //         if (audio) {
-    //             audio.pause(); // Pause audio if the document is hidden
-    //         }
-    //         isPageVisible = false;
-    //     } else {
-    //         isPageVisible = true;
-    //     }
-    // };
-
-    // document.addEventListener('visibilitychange', handleVisibilityChange);
-
-
-    // const getAudio = () => {
-    //     if (!audio) {
-    //         audio = new Audio();
-    //         audio.src = music.getAttribute('data-url');
-    //         audio.load();
-    //         audio.currentTime = 0;
-    //         audio.autoplay = true;
-    //         audio.muted = false;
-    //         audio.loop = true;
-    //         audio.volume = 1;
-    //     }
-
-    //     return audio;
-    // };
-
     const getAudio = () => {
         if (!audio) {
             audio = new Audio();
@@ -66,16 +33,16 @@ export const audio = (() => {
             audio.loop = true;
             audio.volume = 0; // Start with volume set to 0
         }
-    
+
         // Gradually increase volume
         const interval = setInterval(() => {
             if (audio.volume < 1) {
-                audio.volume += 0.1; // Increase volume by 10% each time
+                audio.volume = Math.min(audio.volume + 0.1, 1); // Increase volume by 10% each time, but ensure it doesn't exceed 1
             } else {
                 clearInterval(interval); // Stop increasing volume when it reaches 100%
             }
         }, 1000); // Adjust the interval time (in milliseconds) as needed
-    
+
         return audio;
     };
 
